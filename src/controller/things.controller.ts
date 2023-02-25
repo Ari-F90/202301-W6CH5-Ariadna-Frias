42ab51bddf74992907af653a32a99cb07e903c99
Ariadna Frias <ariadna.812@gmail.com>
2023-02-25 20:07:14 +0100

    Update things controller file

--- src/controller/things.controller.ts
+++ src/controller/things.controller.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable no-unused-vars */
 import { Response, Request, response } from 'express';
+import { Things } from '../models/models.js';
 import { ThingsFileRepo } from '../repository/things.file.repo.js';
 
 export class ThingsController {
@@ -13,10 +14,32 @@ export class ThingsController {
   }
 
   get(req: Request, resp: Response) {
-    this.repo.readById(Number(req.params.id)).then((data) => resp.json(data));
+    this.repo.read().then((data) => {
+      const { id } = req.params;
+      const infoId = data.find((item) => item.id === Number(id));
+      resp.json(infoId);
+    });
   }
 
-  post(_req: Request, _resp: Response) {}
-  patch(_req: Request, _resp: Response) {}
-  delete(_req: Request, _resp: Response) {}
+  post(req: Request, resp: Response) {
+    this.repo.write(req.body).then((data) => {
+      resp.send('Added');
+    });
+  }
+
+  async patch(req: Request, resp: Response) {
+    const id = Number(req.params.id);
+    const prevThing: any = await this.repo.readById(id);
+    const newThing = req.body;
+    const updateThing = Object.assign(prevThing, newThing);
+    await this.repo.update(updateThing);
+    resp.send('Updated');
+  }
+
+  delete(req: Request, resp: Response) {
+    const { id } = req.params;
+    this.repo.delete(Number(id)).then((data) => {
+      resp.send('Deleted');
+    });
+  }
 }
