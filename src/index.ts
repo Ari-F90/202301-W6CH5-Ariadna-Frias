14fe983b022da03c62cdf87a5aefe3cb2359fdf4
Ariadna Frias <ariadna.812@gmail.com>
2023-02-27 17:58:49 +0100

    Update index ts file

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import { app } from './app.js';
 const PORT = process.env.PORT || 4500;
 const server = http.createServer(app);
 
-server.on('error', () => {});
+server.on('error', () => {
+  console.log('Error en el server');
+});
 
 server.on('listening', () => {
   console.log('Listening in http://localhost:' + PORT);
