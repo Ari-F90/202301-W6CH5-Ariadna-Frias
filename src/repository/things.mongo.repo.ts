367ec92920a5a29fb279d255867e9da275fff136
Ariadna Frias <ariadna.812@gmail.com>
2023-02-28 19:04:12 +0100

    Add mongo repo

---
+++ src/repository/things.mongo.repo.ts
@@ -0,0 +1,51 @@
+import { Thing } from '../entities/thing.js';
+import { HTTPError } from '../errors/errors.js';
+import { Repo } from './repo.interface.js';
+import { ThingModel } from './things.mongo.model.js';
+import createDebug from 'debug';
+const debug = createDebug('W6:repo');
+
+export class ThingsMongoRepo implements Repo<Thing> {
+  constructor() {
+    debug('Instantiated');
+  }
+
+  async query(): Promise<Thing[]> {
+    debug('query');
+    const data = await ThingModel.find();
+    return data;
+  }
+
+  async queryId(id: string): Promise<Thing> {
+    debug('queryId');
+    const data = await ThingModel.findById(id);
+    if (!data) throw new HTTPError(404, 'Not found', 'Id not found in queryId');
+    return data;
+  }
+
+  async create(info: Partial<Thing>): Promise<Thing> {
+    debug('create');
+    const data = await ThingModel.create(info);
+    return data;
+  }
+
+  async update(info: Partial<Thing>): Promise<Thing> {
+    debug('update');
+    const data = await ThingModel.findByIdAndUpdate(info._id, info, {
+      new: true,
+    });
+    if (!data) throw new HTTPError(404, 'Not found', 'Id not found in update');
+    return data;
+  }
+
+  async delete(id: string): Promise<void> {
+    debug('delete');
+    const data = await ThingModel.findByIdAndDelete(id);
+    if (!data)
+      throw new HTTPError(
+        404,
+        'Not found',
+        'Delete not possible: id not found'
+      );
+  }
+}
