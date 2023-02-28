7797c02efe33e5968c3cc5cf694f6d6801fb708a
Ariadna Frias <ariadna.812@gmail.com>
2023-02-28 19:02:57 +0100

    Update app ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { thingsRouter } from './routers/things.router.js';
+import createDebug from 'debug';
+import { CustomError } from './errors/errors.js';
 
+const debug = createDebug('W6:app');
 export const app = express();
 app.disable('x-powered-by');
 
@@ -13,4 +16,31 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-app.use('/things', thingsRouter);
+app.use('/thingschallenge', thingsRouter);
+
+app.get('/', (_req, resp) => {
+  resp.json({
+    info: 'Things project app',
+    endpoints: {
+      things: '/thingschallenge',
+    },
+  });
+});
+
+app.use(
+  (error: CustomError, _req: Request, resp: Response, _next: NextFunction) => {
+    debug('Soy el middleware de errores');
+    const status = error.statusCode || 500;
+    const statusMessage = error.statusMessage || 'Internal server error';
+    resp.status(status);
+    resp.json({
+      error: [
+        {
+          status,
+          statusMessage,
+        },
+      ],
+    });
+    debug(status, statusMessage, error.message);
+  }
+);
