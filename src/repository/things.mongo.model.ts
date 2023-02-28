7726e2d79ab0d8042b29afb54eaea433043814b7
Ariadna Frias <ariadna.812@gmail.com>
2023-02-28 22:23:55 +0100

    Update things model

--- src/repository/things.mongo.model.ts
+++ src/repository/things.mongo.model.ts
@@ -1,4 +1,4 @@
-import { model, Schema, SchemaTypes } from 'mongoose';
+import { model, Schema } from 'mongoose';
 import { Thing } from '../entities/thing.js';
 
 const thingSchema = new Schema<Thing>({
