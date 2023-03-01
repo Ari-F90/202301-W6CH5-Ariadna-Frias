d6e04bccce392008b9ab327aae24ece2cf11fa2c
Ariadna Frias <ariadna.812@gmail.com>
2023-03-01 19:30:24 +0100

    Update app ts file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { thingsRouter } from './routers/things.router.js';
 import createDebug from 'debug';
 import { CustomError } from './errors/errors.js';
+import { usersRouter } from './routers/users.router.js';
 
 const debug = createDebug('W6:app');
 export const app = express();
@@ -17,12 +18,14 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 app.use('/thingschallenge', thingsRouter);
+app.use('/users', usersRouter);
 
 app.get('/', (_req, resp) => {
   resp.json({
     info: 'Things project app',
     endpoints: {
       things: '/thingschallenge',
+      users: '/users',
     },
   });
 });
